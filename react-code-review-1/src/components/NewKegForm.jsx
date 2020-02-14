e3e41f965ebe3c260a7373f637ef0977baa792a3
Marcus Jensen <marcusjensen1515@gmail.com>
2020-02-14 14:21:04 -0800

    add addtional form configuration

--- react-code-review-1/src/components/NewKegForm.jsx
+++ react-code-review-1/src/components/NewKegForm.jsx
@@ -14,39 +14,64 @@ export default function NewKegForm(){
     fontSize: '1.5vh'
   }
   const newBeerHeader = {
-    marginBottom: '6vh'
+    marginBottom: '4vh'
+  }
 
+  const formWrap = {
+    backgroundColor: '#E6F9D0',
+    border: 'thick solid #D8F2BB',
+    borderRadius: '20px',
+    padding: '0 40px',
+    boxShadow: '-2px 10px 20px #40403F',
+    color: 'navy',
+    marginLeft: '30%',
+    marginRight: '30%',
+    marginTop: '6%'
   }
 
   return(
 
     <div style={textCenter}>
-      <h1 style={newBeerHeader}>Add a new beer </h1>
-      <form>
-        <label>
-          Name of Beer:
-          <input type="text" name="name" />
-        </label>
-        <label>
-          Brand:
-          <input type="text" name="name" />
-        </label>
-        <label>
-          Price per Pint:
-          <input type="text" name="name" />
-        </label>
-        <label>
-          Alcohol Content:
-          <input type="text" name="name" />
-        </label>
-        <label>
-          Keg Volume:
-          <input type="number" name="name" />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
+      <div style={formWrap}>
+        <h1 style={newBeerHeader}>Add a new beer </h1>
+        <form>
+          <ul>
+            <li>
+              <label>
+                Name of Beer:
+                <input type="text" name="name" />
+              </label>
+            </li>
+            <li>
+              <label>
+                Brand:
+                <input type="text" name="name" />
+              </label>
+            </li>
+            <li>
+              <label>
+                Price per Pint:
+                <input type="text" name="name" />
+              </label>
+            </li>
+            <li>
+              <label>
+                Alcohol Content:
+                <input type="text" name="name" />
+              </label>
+            </li>
+            <li>
+              <label>
+                Keg Volume:
+                <input type="number" name="name" />
+              </label>
+            </li>
+            <li><input type="submit" value="Submit" /></li>
 
+          </ul>
+        </form>
 
+      </div>
     </div>
   );
 }
